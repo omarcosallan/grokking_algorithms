10507c5be2c8ff1eca78b987a37b5e522772d665
Marcos Allan <marcosallanm1to@gmail.com>
2025-10-06 18:55:28 -0300

    code for chapter 2 in java: selection sort v2

---
+++ 02_selection_sort/src/main/SelectionSort2.java
@@ -0,0 +1,27 @@
+package main;
+
+import java.util.Arrays;
+
+public class SelectionSort2 {
+
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 6, 2, 10};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr)); // [2, 3, 5, 6, 10]
+    }
+
+    // O(n * n) => O(n^2)
+    private static int[] selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[i]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+}
